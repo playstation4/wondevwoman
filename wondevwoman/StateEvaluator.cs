6846a8d1edb93e4ff42adf2ea5296faafae0e55d
Андрей Шельпяков <andreyshelpyakov@gmail.com>
2018-03-06 00:28:25 +0500

    GreedyAi

--- wondevwoman/StateEvaluator.cs
+++ wondevwoman/StateEvaluator.cs
@@ -7,9 +7,23 @@ namespace CG.WondevWoman
 {
     public class StateEvaluator : IStateEvaluator
     {
+        private const int ScoreCoefficent = 100;
+        private const int HeightCoefficent = 50;
+        private const int AvailableActionsCoefficent = 12;
+        
+
         public ExplainedScore Evaluate(State state, int playerIndex)
         {
-            return 42;
+            //var oldPlayer = state.CurrentPlayer;
+            //if(state.CurrentPlayer != playerIndex)
+            //    state.ChangeCurrentPlayer();
+            //var result = ScoreCoefficent * state.GetScore(playerIndex) +
+            //             HeightCoefficent * state.GetUnits(playerIndex).Where(x => x.X >= 0).Average(state.HeightAt) +
+            //             AvailableActionsCoefficent * state.GetPossibleActions().Count;
+            //if (state.CurrentPlayer != oldPlayer)
+            //    state.ChangeCurrentPlayer();
+            var result = 0.1 * state.GetPossibleActions().Count + state.MyUnits.Sum(state.HeightAt);
+            return result;
         }
 
     }
